d228f2baa9a1cc4f4bb45199051e96dd7b381a50
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-10-29 07:32:37 +0300

    Created Views for the Pomodoro tracker

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,33 @@
 from flask import render_template,request,redirect,url_for,abort
+from . import main
 from ..models import Todo,User
 from .forms import TodoForm, UpdateProfile
 import db,photos
+from flask_login import login_required,current_user
 
+
+@main.route('/')
+def index():
+    '''
+    View root page that returns the index page and its data
+    '''
+    title = 'Pomodoro Tracker'
+    render_template('index.html', title=title)
+
+@main.route('/new_todo', methods=['GET','POST'])
+@login_required
+def new_todo():
+    form = TodoForm()
+    if form.validate_on_submit():
+        category = form.category.data
+        description = form.description.data
+        user_id = current_user
+        new_todo= Todo(category=category,description=description,user_id=current_user._get_current_object().id)
+        new_todo.save_todos()
+
+        return redirect(url_for('main.index'))
+    return render_template('new_todo.html', form=form)
+    
 @main.route('/user/<uname>')
 def profile(uname):
     user = User.query.filter_by(username = uname).first()
